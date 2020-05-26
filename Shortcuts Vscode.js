02fd51d01f80ef71ce1d43c0cef11c88e0ee0c5b
gache <erickfrancodelgado@hotmail.com>
2020-05-26 16:19:58 +0200

    Pour créer un fichier

--- Shortcuts Vscode.js
+++ Shortcuts Vscode.js
@@ -10,3 +10,10 @@
 //  Ctrl + /
 // pour commenter un parti du code 
 // Ctrl + alt + A
+
+
+/* Pour créer un fichier */
+
+//  si j'ai une route, par contre le ficher n'est pas encore créer  je fais 
+// Ctrl + click 
+//  une fênetre s'ouvre pour créer le fichier
