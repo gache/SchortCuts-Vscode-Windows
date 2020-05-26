21c7ec316e6dae98cb0ee1abff9abba03029ef20
gache <erickfrancodelgado@hotmail.com>
2020-05-26 16:29:46 +0200

    Effacer une ligne de code

--- Shortcuts Vscode.js
+++ Shortcuts Vscode.js
@@ -3,7 +3,7 @@
 //  si le document va trop à la droite  on fait
 //   Shift + tab
 
-
+/***********************************************/
 
  /* pour commenter le code */
 
@@ -11,9 +11,31 @@
 // pour commenter un parti du code 
 // Ctrl + alt + A
 
-
+/***********************************************/
 /* Pour créer un fichier */
 
 //  si j'ai une route, par contre le ficher n'est pas encore créer  je fais 
 // Ctrl + click 
 //  une fênetre s'ouvre pour créer le fichier
+
+/***********************************************/
+/* pour aller vers les definitions d'une variable function etc */
+
+// // Ctrl + click 
+
+/***********************************************/
+/* Effacer une ligne de code */
+
+// Ctrl + Shift + L  cette commande fait une selecction des mots pareil
+// Ctrl + alt +  K  efface le code seleccioné
+
+/***********************************************/
+
+/* venir en arrière et en avant */
+
+// Ctrl +Z cette commande va en arrière
+// Ctrl + Shift + Z cette commande va en avant
+
+
+
+
