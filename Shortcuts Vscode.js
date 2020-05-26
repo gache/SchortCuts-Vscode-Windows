25c90b2e64eef0d2d2ba50b537b702cc5f6c7ddf
gache <erickfrancodelgado@hotmail.com>
2020-05-26 16:14:35 +0200

    commenter le code

--- Shortcuts Vscode.js
+++ Shortcuts Vscode.js
@@ -1,2 +1,12 @@
-// Mouvement  avec le code pour le mieux organiser
+/* Mouvement  avec le code pour le mieux organiser */
 //  Alt + ↑ / ↓
+//  si le document va trop à la droite  on fait
+//   Shift + tab
+
+
+
+ /* pour commenter le code */
+
+//  Ctrl + /
+// pour commenter un parti du code 
+// Ctrl + alt + A
